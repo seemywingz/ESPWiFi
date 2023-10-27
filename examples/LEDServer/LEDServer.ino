390bb5d06f4ce7a3d429a79f59fc06055f195ef6
Kevin Jayne <kevin.jayne@icloud.com>
2023-10-27 01:12:17 -0400

    led server

--- examples/LEDServer/LEDServer.ino
+++ examples/LEDServer/LEDServer.ino
@@ -4,8 +4,8 @@
 #include "indexHTML.h"
 
 // setup the wifi
-String ssid = "YOU_SSID";           // your network SSID (name)
-String password = "YOUR_PASSWORD";  // your network password
+String ssid = "ESPWiFiAP";     // your network SSID (name)
+String password = "12345678";  // your network password
 ESPWiFi espWiFi(ssid, password);
 
 void setup() {
@@ -33,59 +33,15 @@ void toggleLED() {  // toggle the LED
 }
 
 void initHandlers() {  // initialize the handlers
+  espWiFi.webserver()->on("/led/toggle", []() { toggleLED(); });
   espWiFi.webserver()->on("/", HTTP_GET, []() {
     espWiFi.webserver()->send(200, "text/html", indexHTML);
   });
-
-  espWiFi.webserver()->on("/led/toggle", []() { toggleLED(); });
-
-  espWiFi.webserver()->onNotFound(
-      []() { espWiFi.webserver()->send(404, "text/html", boardInfo()); });
-}
-
-String boardInfo() {  // return the board info as a HTML string
-  String message = "<html><body>";
-  message += "<h1>Board Information</h1>";
-  message += "<p>URI: " + espWiFi.webserver()->uri() + "</p>";
-  message +=
-      "<p>Method: " +
-      String(espWiFi.webserver()->method() == HTTP_GET ? "GET" : "POST") +
-      "</p>";
-  message += "<h2>Request Parameters</h2>";
-  message += "<ul>";
-  for (uint8_t i = 0; i < espWiFi.webserver()->args(); i++) {
-    message += "<li>" + espWiFi.webserver()->argName(i) + ": " +
-               espWiFi.webserver()->arg(i) + "</li>";
-  }
-  message += "</ul>";
-  message += "<h2>Board Details</h2>";
-  message += "<ul>";
-  message +=
-      "<li><strong>Board Type:</strong> NodeMcu Mini Wireless D1 Module "
-      "(ESP8266 ESP-12F)</li>";
-  message += "<li><strong>Firmware Version:</strong> " +
-             String(ESP.getCoreVersion()) + "</li>";
-  message += "<li><strong>SDK Version:</strong> " +
-             String(ESP.getSdkVersion()) + "</li>";
-  message += "<li><strong>Flash Chip ID:</strong> " +
-             String(ESP.getFlashChipId(), HEX) + "</li>";
-  message += "<li><strong>Flash Chip Size:</strong> " +
-             String(ESP.getFlashChipSize() / (1024 * 1024)) + " MB</li>";
-  message += "<li><strong>Free Heap:</strong> " + String(ESP.getFreeHeap()) +
-             " bytes</li>";
-  message +=
-      "<li><strong>Chip ID:</strong> " + String(ESP.getChipId(), HEX) + "</li>";
-  message += "<li><strong>CPU Frequency:</strong> " +
-             String(ESP.getCpuFreqMHz()) + " MHz</li>";
-  message += "<li><strong>Flash Frequency:</strong> " +
-             String(ESP.getFlashChipSpeed() / 1000000) + " MHz</li>";
-  message += "<li><strong>Flash Mode:</strong> " +
-             String(ESP.getFlashChipMode()) + "</li>";
-  message += "</ul>";
-  message += "<h2>LED State</h2>";
-  message += "<p>" +
-             String((digitalRead(LED_BUILTIN) == HIGH) ? "LED off" : "LED on") +
-             "</p>";
-  message += "</body></html>";
-  return message;
+  espWiFi.webserver()->on("/led/state", HTTP_GET, []() {
+    if (digitalRead(LED_BUILTIN) == HIGH) {
+      espWiFi.webserver()->send(200, "text/plain", "LED off");
+    } else {
+      espWiFi.webserver()->send(200, "text/plain", "LED on");
+    }
+  });
 }
--- examples/LEDServer/indexHTML.h
+++ examples/LEDServer/indexHTML.h
@@ -1,7 +1,6 @@
 const char indexHTML[] PROGMEM = R"rawliteral(
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>LED Web Controls</title>
@@ -14,7 +13,6 @@ const char indexHTML[] PROGMEM = R"rawliteral(
             padding: 0;
             color: #fff;
         }
-
         .container {
             display: flex;
             flex-direction: column;
@@ -22,12 +20,10 @@ const char indexHTML[] PROGMEM = R"rawliteral(
             justify-content: center;
             height: 100vh;
         }
-
         .header {
             font-size: 36px;
             margin-bottom: 20px;
         }
-
         .button-container {
             background-color: #111;
             padding: 20px;
@@ -35,10 +31,8 @@ const char indexHTML[] PROGMEM = R"rawliteral(
             margin: 20px;
             transition: box-shadow 0.3s ease;
         }
-
         .button {
             background-color: #444;
-            /* Medium Dark Gray */
             border: none;
             color: #fff;
             padding: 20px 40px;
@@ -51,144 +45,56 @@ const char indexHTML[] PROGMEM = R"rawliteral(
             border-radius: 15px;
             transition: background-color 0.3s ease, box-shadow 0.3s ease;
         }
-
         .button:hover {
             background-color: #555;
-            /* Darker Gray on Hover */
-            box-shadow: 0px 5px 15px 0px rgba(0, 0, 0, 0.3);
-        }
-
-        .info-button {
-            background-color: #444;
-            /* Medium Dark Gray - Same as Toggle Button */
-        }
-
-        .info-container {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background-color: rgba(0, 0, 0, 0.5);
-            display: none;
-            align-items: center;
-            justify-content: center;
-            z-index: 1;
-        }
-
-        .info-box {
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 15px;
-            box-shadow: 0px 5px 15px 0px rgba(0, 0, 0, 0.3);
-            max-width: 80%;
-            max-height: 80%;
-            overflow: auto;
-            font-size: 18px;
-            line-height: 1.5;
-            color: #333;
-            /* Dark Grey Text Color */
-        }
-
-        .close-button,
-        .info-button {
-            position: absolute;
-            top: 10px;
-            right: 10px;
-            background-color: #444;
-            /* Medium Dark Gray - Same as Toggle Button */
-            border: none;
-            color: #fff;
-            padding: 10px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 18px;
-            margin: 10px;
-            cursor: pointer;
-            border-radius: 10px;
-            transition: background-color 0.3s ease, box-shadow 0.3s ease;
-        }
-
-        .close-button:hover,
-        .info-button:hover {
-            background-color: #555;
-            /* Darker Gray on Hover - Same as Toggle Button */
             box-shadow: 0px 5px 15px 0px rgba(0, 0, 0, 0.3);
         }
     </style>
 </head>
-
 <body>
     <div class="container">
         <div class="header">LED Web Controls</div>
         <div class="button-container">
             <button id="toggleButton" class='button'>Loading...</button>
-            <button id="infoButton" class='button info-button'>Info</button>
-        </div>
-    </div>
-
-    <div class="info-container" id="infoContainer">
-        <div class="info-box">
-            <button class="close-button" id="closeButton">Close</button>
-            <p id="boardInfo"></p>
         </div>
     </div>
 
     <script>
-        var button = document.getElementById('toggleButton');
-        var infoButton = document.getElementById('infoButton');
-        var infoContainer = document.getElementById('infoContainer');
-        var closeButton = document.getElementById('closeButton');
-        var boardInfo = document.getElementById('boardInfo');
+    var button = document.getElementById('toggleButton');
 
-        function updateButton(isLedOn) {
-            button.textContent = 'Turn ' + (isLedOn ? 'Off' : 'On');
-        }
+    function updateButton(isLedOn) {
+        button.textContent = 'Turn ' + (isLedOn ? 'Off' : 'On');
+    }
 
-        function toggleLED() {
-            fetch('/led/toggle', { method: 'GET' })
-                .then(response => response.text())
-                .then(data => {
-                    console.log(data);
-                    var isLedOn = data.includes('on'); // Check if the response contains 'on'
-                    updateButton(isLedOn);
-                });
-        }
-
-        // Fetch the initial state of the LED when the page is loaded
-        fetch('/led/state', { method: 'GET' })
+    function toggleLED() {
+        console.log("Button clicked"); // Debugging line
+        fetch('/led/toggle', { method: 'GET' })
             .then(response => response.text())
             .then(data => {
-                var isLedOn = data.includes('on'); // Check if the response contains 'on'
+                console.log("Response:", data); // Debugging line
+                var isLedOn = data.toLowerCase().includes('on'); // Case-insensitive check
                 updateButton(isLedOn);
-                // Attach click event listener after setting the initial button state
-                button.addEventListener('click', toggleLED);
             })
             .catch(error => {
-                console.error('Error fetching LED state:', error);
+                console.error('Error toggling LED:', error);
+                button.textContent = `Error: ${error.message}`;
             });
+    }
 
-        // Show the info container when the info button is clicked
-        infoButton.addEventListener('click', function () {
-            fetch('/info', { method: 'GET' })
-                .then(response => response.text())
-                .then(data => {
-                    // Replace newline characters with <br> elements for proper line breaks
-                    boardInfo.innerHTML = data.replace(/\n/g, '<br>');
-                    infoContainer.style.display = 'flex';
-                })
-                .catch(error => {
-                    console.error('Error fetching board info:', error);
-                });
-        });
-
-        // Hide the info container when the close button is clicked
-        closeButton.addEventListener('click', function () {
-            infoContainer.style.display = 'none';
+    // Fetch the initial state of the LED when the page is loaded
+    fetch('/led/state', { method: 'GET' })
+        .then(response => response.text())
+        .then(data => {
+            var isLedOn = data.toLowerCase().includes('on'); // Case-insensitive check
+            updateButton(isLedOn);
+            // Attach click event listener after setting the initial button state
+            button.addEventListener('click', toggleLED);
+        })
+        .catch(error => {
+            console.error('Error fetching LED state:', error);
+            button.textContent = 'Error';
         });
     </script>
 </body>
-
 </html>
 )rawliteral";
