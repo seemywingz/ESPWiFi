fc64f6efa2d2b7ee035996b587e450df494904c7
seemywings <kevin.jayne@icloud.com>
2023-12-25 15:12:20 -0500

    esp32 updates

--- ESPWiFi.h
+++ ESPWiFi.h
@@ -2,10 +2,10 @@
 #define ESP_WIFI_H
 
 #include <Arduino.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266mDNS.h>
+#include <ESPmDNS.h>
 #include <LittleFS.h>
+#include <WebServer.h>
+#include <WiFi.h>
 
 class ESPWiFi {
  private:
@@ -36,7 +36,7 @@ class ESPWiFi {
   int findBestChannel();
 
  public:
-  ESP8266WebServer webServer;
+  WebServer webServer;
   String indexHTMLFile = "/index.html";
   String wifiCredentialsFile = "/wifi-credentials";
   ESPWiFi(String defaultSSID, String defaultPassword);
