8c487a849af9857a7438b0aeb9ad075b421b6fa7
seemywings <kevin.jayne@icloud.com>
2024-05-05 15:20:18 -0400

    dist-upload

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -43,6 +43,7 @@ function App() {
     }).then(data => {
       setConfig(newConfig);
       console.log('Configuration updated:', data);
+      alert('Configuration updated');
     }).catch(error => console.error('Error updating configuration:', error));
   };
 
